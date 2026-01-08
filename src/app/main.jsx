0270d101f26b78b8e16230165131447427db1892
Dragan Milijasevic <dragan.rs3@gmail.com>
2026-01-08 15:38:33 +0100

    Add caching for api calls

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
 import HomePage from '@/pages/HomePage';
 import PokemonPage from '@/pages/PokemonPage';
 import { createBrowserRouter } from 'react-router-dom';
